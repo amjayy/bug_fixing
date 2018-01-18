5a94a68713a9de9e86cb84c08b210218a458357a
amjayy <33906054+amjayy@users.noreply.github.com>
2018-01-18 16:41:49 -0600

    Add files via upload

---
+++ mystore.py
@@ -0,0 +1,14 @@
+print("Welcome to B3 Central!")
+print("Hi, what's your name?")
+name=input()
+print("Excellent!"+name)
+print("What is your zipcode?")
+zipcode=input()
+print("Fantastic!")
+print(name+ "I see others in your area like this product."
+print("Would you like to browse this product's page?")
+if answer == "yes" or == "yeah":
+    print("Click the link below")
+if answer == in ("no", "maybe later")
+    print ("No worries! What would you like to view first?")
+    
---
+++ rock_papper_scissors.py
@@ -0,0 +1,21 @@
+from random import randint
+
+print("Welcome to Rock,Paper, Scissors")
+
+def make_a_choice():
+     choice = ""
+     while choice != "r"and choice  != "s" and choice != "p":
+          choice = input( "Player, please make your choice: Rock(r), Scissors(s), or Paper(p):")
+          print()
+
+     return choice
+
+def computer_turn():
+    turn = ""
+    
+
+make_a_choice()
+
+    
+
+    
